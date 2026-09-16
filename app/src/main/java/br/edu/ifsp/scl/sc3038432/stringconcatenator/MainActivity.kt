fa3b1a1819457be04db45ddd1c970c299ddcb27c
hikazudani <lhjundi@proton.me>
2026-09-16 16:40:52 -0300

    feat: setup main activity nav host

--- app/src/main/java/br/edu/ifsp/scl/sc3038432/stringconcatenator/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/stringconcatenator/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import br.edu.ifsp.scl.sc3038432.stringconcatenator.navigation.MainNavHost
 import br.edu.ifsp.scl.sc3038432.stringconcatenator.ui.theme.StringConcatenatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +17,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            val navHostController = rememberNavController()
+
             StringConcatenatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainNavHost(
+                        navHostController = navHostController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StringConcatenatorTheme {
-        Greeting("Android")
     }
 }
